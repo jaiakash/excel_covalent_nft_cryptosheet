a1aabfd4e56c1e70de1dd24e55dc83c65d66fb7f
Akash Jaiswal <akashjaiswal3846@gmail.com>
2022-03-09 15:39:06 +0530

    add: sort code

--- Code.js
+++ Code.js
@@ -1,12 +1,40 @@
 // custom menu
+var sortVar = 1;
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
   ui.createMenu("NFT Cryptosheet")
     .addItem("Get NFT Data", "displayNFTData")
     .addToUi();
+  ui.createMenu("Sort the Data")
+    .addItem("by Name", "sortN")
+    .addItem("by Volume", "sortV")
+    .addItem("by Transaction Count", "sortTC")
+    .addItem("by Market Capital", "sortMC")
+    .addItem("by Max Price", "sortMP")
+    .addToUi();
+}
+function sortN() {
+  sortVar = 1;
+  displayNFTData();
+}
+function sortV() {
+  sortVar = 3;
+  displayNFTData();
+}
+function sortMC() {
+  sortVar = 4;
+  displayNFTData();
+}
+function sortTC() {
+  sortVar = 5;
+  displayNFTData();
+}
+function sortMP() {
+  sortVar = 6;
+  displayNFTData();
 }
 
-function callCovalentAPI(artist) {
+function callCovalentAPI() {
   // Call the Covalent API
   var response = UrlFetchApp.fetch(
     "https://api.covalenthq.com/v1/1/nft_market/?key=ckey_ca8b6bd524ce4cc4a4d1a28ac19"
@@ -21,8 +49,6 @@ function displayNFTData() {
   // pick up the search term from the Google Sheet
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet = ss.getActiveSheet();
-
-  var artist = sheet.getRange(11, 2).getValue();
 
   var results = callCovalentAPI()["data"]["items"];
 
@@ -40,22 +66,34 @@ function displayNFTData() {
       elem["unique_wallet_purchase_count_alltime"],
       elem["max_price_quote"],
       elem["floor_price_quote_7d"],
-      elem["gas_quote_rate"],
       image,
     ]);
     sheet.setRowHeight(i + 2, 65);
   });
 
+  var sortedOutput = output.sort(function (a, b) {
+    var dataA = a[sortVar] ? a[sortVar] : "Not known";
+    var dataB = b[sortVar] ? b[sortVar] : "Not known";
+
+    if (dataA < dataB) {
+      return 1;
+    } else if (dataA > dataB) {
+      return -1;
+    }
+    // names are equal
+    return 0;
+  });
+  output = sortedOutput;
   var len = output.length;
 
   // clear any previous content
-  sheet.getRange(2, 1, 500, 11).clearContent();
+  sheet.getRange(2, 1, 500, 10).clearContent();
 
   // paste in the values
-  sheet.getRange(2, 1, len, 11).setValues(output);
+  sheet.getRange(2, 1, len, 10).setValues(output);
 
   // formatting
-  sheet.getRange(2, 1, 500, 11).setVerticalAlignment("middle");
+  sheet.getRange(2, 1, 500, 10).setVerticalAlignment("middle");
   sheet.getRange(2, 5, 500, 1).setHorizontalAlignment("center");
   sheet.getRange(2, 2, len, 3).setWrap(true);
 }
